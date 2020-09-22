3a7d528c1a1662d18f40672fe1dc6654c7a50ecd
SamuelGomezCabrera <samuel.gomezc@my.unitec.edu.mx>
2020-09-22 18:39:05 -0500

    Segundo Intento

---
+++ app/src/main/java/com/unitec/proyectounitec/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.unitec.proyectounitec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//El punto de partida de toda app es la MainActivity (Presenter)
+/*
+public lass TarjetaCredito extends Tarjeta{
+public TarjetaCredito(){
+super();
+}
+}
+    : es herencia
+*/
+//AppCompatActivity(): Android 10 hasta Android 5 que fue la que escogimos
+// fun es una palabra reservada para el metodo
+
+class MainActivity : AppCompatActivity() {
+    //savedInstanceState: Bundle? : nombre y tipo de dato
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
